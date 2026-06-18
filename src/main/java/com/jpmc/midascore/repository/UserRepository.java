3bbedede32e1075a9d5dff10093ca519004970f8
Vaibhav Kashyap <vaibhavk200320@gmail.com>
2026-06-18 16:09:28 +0530

    Change UserRepository to extend JpaRepository

--- src/main/java/com/jpmc/midascore/repository/UserRepository.java
+++ src/main/java/com/jpmc/midascore/repository/UserRepository.java
@@ -1,8 +1,5 @@
-package com.jpmc.midascore.repository;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import com.jpmc.midascore.entity.UserRecord;
-import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<UserRecord, Long> {
-    UserRecord findById(long id);
+public interface UserRepository extends JpaRepository<UserRecord, Long> {
 }
